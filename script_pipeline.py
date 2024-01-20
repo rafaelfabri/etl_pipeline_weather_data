6636dcf4a904a56acca7c021f4fde742a7fe8dc6
rafaelfabri <rafael-fabri-chimidt@live.com>
2024-01-20 12:47:51 -0300

    add funcao para atribuir senhas 2

--- script_pipeline.py
+++ script_pipeline.py
@@ -1,6 +1,12 @@
 import requests
+import pandas as pd
 from requests.auth import HTTPBasicAuth
 
+def atribuindo_senha():
+    
+    with open('senhas/senhas.csv', 'r') as f:
+        df = pd.read_csv(f)
+        
 
 query = 'https://api.meteomatics.com/2024-01-18T00:00:00Z--2024-01-21T00:00:00Z:PT1H/t_2m:C/52.520551,13.461804/html'
 
@@ -10,3 +16,7 @@ r = requests.get(query, auth = HTTPBasicAuth('-', '-'))
 
 
 print(r.status_code)
+
+
+if __name__ == "__main__":
+    atribuindo_senha()

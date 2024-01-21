3ff6a929c39303ecfa896e04b7a405afa8666980
rafaelfabri <rafael-fabri-chimidt@live.com>
2024-01-21 11:56:19 -0300

    funcoes add senhas e requisicao de dados prontas

--- script_pipeline.py
+++ script_pipeline.py
@@ -1,24 +1,21 @@
 import requests
 import pandas as pd
 from requests.auth import HTTPBasicAuth
-
+import csv
+import datetime 
 
 
 
 def atribuindo_senha():
     
     try:
-    
-        with open('/home/rafaelfabrichimidt/Documentos/Projetos/Python/codigos/pipeline_api_weather/senhas/senhas.csv') as f:
+        
+        with open('/home/rafaelfabrichimidt/Documentos/Projetos/Python/codigos/pipeline_api_weather/senhas/senhas.csv', 'r') as f:
             
             df = pd.read_csv(f)
             
-            
             login = df.loc[0, 'login']
             senha = df.loc[0, 'senha']
-            
-            print(login)
-            print(senha)
             
             return login, senha
 
@@ -33,15 +30,53 @@ def atribuindo_senha():
     
     
 
-
 def requisicao_dados(login, senha):
 
-    query = 'https://api.meteomatics.com/2024-01-18T00:00:00Z--2024-01-21T00:00:00Z:PT1H/t_2m:C/52.520551,13.461804/html'
+    query = 'https://api.meteomatics.com/2024-01-20T00:00:00Z--2024-01-23T00:00:00Z:PT1H/t_2m:C/52.520551,13.461804/csv'
     
     
-    r = requests.get(query, auth = HTTPBasicAuth(login, senha))
+    try:
+        
+        r = requests.get(query, auth = HTTPBasicAuth(login, senha))
+    
+        if r.status_code == 200:
+            
+            print('Success in request!')
+            
+            data_csv = r.content.decode('utf-8')
+            
+            data_csv = csv.reader(data_csv.splitlines(), delimiter = ';')
+            
+            list_of_data = []
+            
+            for row in data_csv:
+                
+                list_of_data.append(row)
+            
+            df = pd.DataFrame(list_of_data)
+            
+            return df
+            
+        
+        else:
+            
+            print('Resquest denied')
+            
+            df = pd.DataFrame()
+            
+            return df
 
-    print(r.status_code)
+            
+    except:
+        
+        print('error - link error')
+        
+        df = pd.DataFrame()
+            
+        return df
+        
+
+
 
 
 
@@ -50,11 +85,18 @@ if __name__ == "__main__":
     
     login, senha = atribuindo_senha()
         
-    print(login)
+
     
     if login != ' ':
     
-        requisicao_dados(login, senha)
+        df = requisicao_dados(login, senha)
+        
+        if df.shape[0] > 0:
+            
+            print(df)
+        
+        
         
     else:
+        
         print('sem senha')
